be5c6341b286f1c8a55e826798d66c4bd2873508
Jackie He <Jackie.He8@gmail.com>
2018-04-27 17:51:23 +0000

    Original Files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("button").click(function()){
+        var enter = $(".input-group").val()
+        if (enter.toLowerCase() == "mother knows best") {
+            
+        } else {
+            
+        };
+    };
+});
